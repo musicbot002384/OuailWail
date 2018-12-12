94015bcc292f39466207d2e1d43f1cf737481220
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-12 18:35:56 +0100

    Update test.js

--- test.js
+++ test.js
@@ -52,7 +52,7 @@ if (message.content.startsWith(prefix + 'setgame')) {
 
 
 client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome');
+    let channel = member.guild.channels.find('name', 'chat');
     let memberavatar = member.user.avatarURL
       if (!channel) return;
     let embed = new Discord.RichEmbed()
