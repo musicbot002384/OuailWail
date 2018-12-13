53c7382f1425489cd897eece39b232c0c070a781
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-14 00:37:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -369,7 +369,7 @@ client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
     const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "welcome");
      stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+     stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   });
 })
 
