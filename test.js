1e28aa7a7dda01b5c0ccb0ba299a63402b736fc4
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-15 14:36:46 +0100

    Update test.js

--- test.js
+++ test.js
@@ -215,7 +215,39 @@ client.on('message', message => {
   }
 });
 
-
+client.on('guildMemberAdd', (member) => {
+  member.addRole(member.guild.roles.find('name', 'not active'));
+  });
+   
+   
+  client.on('message', message => {                      
+      if(!message.channel.guild) return;
+         if(message.content.startsWith(prefix + 'active')) {
+          let modlog = client.channels.find('name', 'الـــــــــشات_العام');
+         if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+         message.channel.sendMessage(`اضغط على الصح عشان تتفعل`).then(msg => {
+         
+         
+          msg.react('✅')
+         .then(() => msg.react('✅'))
+       
+       
+   
+         let activeFilter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+       
+         let active = msg.createReactionCollector(activeFilter, { time: 15000 });
+       
+                                                         
+                                 active.on("collect", r => {
+                                     message.member.addRole(message.guild.roles.find("name", "active"));
+                                     message.member.removeRole(message.guild.roles.find("name", "not active"));
+                                     msg.delete();
+                                     message.channel.send(`**تم تفعيلك استمتع.**`).then(m => m.delete(1000));
+       
+                                     })
+                                     })
+                                     }
+                                     });
 
 client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
   member.guild.fetchInvites().then(guildInvites => {
