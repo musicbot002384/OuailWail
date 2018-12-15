582d187f5efc4c14dcbeed49046b96c7c75fe5ea
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-15 11:32:46 +0100

    Update test.js

--- test.js
+++ test.js
@@ -54,33 +54,46 @@ if (message.content.startsWith(prefix + 'setgame')) {
 
 });
 
- client.on('message', message => {
-if (message.author.id === client.user.id) return;
-if (message.guild) {
-let embed = new Discord.RichEmbed()
-let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc2') {
-if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
-if (!args[1]) {
-return;
-}
-  message.guild.members.forEach(m => {
-if(!message.member.hasPermission('ADMINISTRATOR')) return;
-      var bc = new Discord.RichEmbed()
-      .addField('# | الرسالة ', args)
-      .setThumbnail(message.guild.iconURL)
-      .setColor('RANDOM')
-      m.sendMessage(args)
-  });
-         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply(":x: **ليس لديك صلاحية للنشر هنا**");
-  const AziRo = new Discord.RichEmbed()   
-  .setColor('RANDOM')
-  message.channel.sendEmbed(AziRo);          
-}
-} else {
-  return;
-}
-});
+client.on('message', message => {
+  if(!message.channel.guild) return;
+if(message.content.startsWith('%bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );  //7md
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "DgPro-BC";
+let request = `Requested By ${message.author.username}`;  //7md
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))  //7md
+.then(() =>msg.react('✅'))  //7md
+
+
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });  //7md
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });  //7md
+reaction1.on("collect", r => {
+message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));  //7md
+message.guild.members.forEach(m => {
+var bc = new  
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  //7md
+.addField('الرساله', args)  //7md
+.setThumbnail(message.author.avatarURL)  //7md
+.setFooter(copy, client.user.avatarURL); //7md
+m.send({ embed: bc })
+msg.delete();  //7md
+})
+})
+reaction2.on("collect", r => {  //7md
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));   //7md
+msg.delete();  //7md  
+})  //7md
+}) //7md
+}  //7md
+}) //7md
 
 client.on('message', message=> {
   if (message.author.bot) return;
