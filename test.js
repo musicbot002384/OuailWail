a33666018c390ee4eb74c0aeeac421824173ab6e
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-15 12:41:52 +0100

    Update test.js

--- test.js
+++ test.js
@@ -193,43 +193,7 @@ client.on('guildCreate', guild => {
       guild.owner.send(embed)
 });
 
-client.on('guildMemberAdd', member => {
-  let channel = member.guild.channels.find('name', 'welcome');
-  let memberavatar = member.user.avatarURL
-    if (!channel) return;
-  let embed = new Discord.RichEmbed()
-      .setColor('PURPLE')
-      .setThumbnail(memberavatar)
-      .addField('🎽 | name :  ',`${member}`)
-      .addField('📢 | اطلق من دخل' , `Welcome to the server, ${member}`)
-      .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
-              .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-             
-                .addField("دخل من طرف:",`<@` + `${inviter.id}` + `>`, true)
-                   
-                                   .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                     
-   .setFooter(`${member.guild.name}`)
-      .setTimestamp()
- 
-    channel.sendEmbed(embed);
-  });
- 
-  client.on('guildMemberRemove', member => {
-      var embed = new Discord.RichEmbed()
-      .setAuthor(member.user.username, member.user.avatarURL)
-      .setThumbnail(member.user.avatarURL)
-      .setTitle(`بس بعرف وين رحت؟؟؟ :raised_hand::skin-tone-1: :pensive:`)
-      .setDescription(`مع السلامه تشرفنا بك :raised_hand::skin-tone-1: :pensive: `)
-      .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-      .setColor('PURPLE')
-      .setFooter(`====اهلا السيرفر نور بيك و الله====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
- 
-  var channel =member.guild.channels.find('name', 'welcome')
-  if (!channel) return;
-  channel.send({embed : embed});
 
-  });
 
 client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
   member.guild.fetchInvites().then(guildInvites => {
