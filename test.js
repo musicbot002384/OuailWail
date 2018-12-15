e3ebc59499f8ed34734130d31ee11b8e1ff77b55
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-15 23:34:04 +0100

    Update test.js

--- test.js
+++ test.js
@@ -246,7 +246,6 @@ client.on('guildMemberAdd', member => {
     const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "s_chat");
      stewart.send(`<@${member.user.id}> دخل للسيرفر من طرف  <@${inviter.id}>`);
-     stewart.send(`<@${members.user.createdAt}> صنع في <@${inviter.createdAt}>`);
     // stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
 });
