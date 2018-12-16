cc870b4cfa88dd291303519a0fd4d09fe1886410
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-16 22:43:17 +0100

    Update test.js

--- test.js
+++ test.js
@@ -73,7 +73,7 @@ client.on('message', message=> {
   if (message.author.bot) return;
   if (message.isMentioned(client.user))
   {
-  message.reply(" :shark: هلا انا بوت سيرفر شاركس");
+  message.reply(" البوت قيد العمل");
   }
 });
 
@@ -215,7 +215,7 @@ client.on('guildMemberAdd', (member) => {
                                      message.member.addRole(message.guild.roles.find("name", "Shark"));
                                      message.member.removeRole(message.guild.roles.find("name", "not active"));
                                      msg.delete();
-                                     message.channel.send(`**تم تفعيلك .**`).then(m => m.delete(1000));
+                                     message.channel.send(`**تم تفعيلك استمتع.**`).then(m => m.delete(1000));
        
                                      })
                                      })
@@ -230,7 +230,7 @@ const invites = {};
 const wait = require('util').promisify(setTimeout);
 
 client.on('ready', () => {
-  wait(100);
+  wait(0);
 
   client.guilds.forEach(g => {
     g.fetchInvites().then(guildInvites => {
