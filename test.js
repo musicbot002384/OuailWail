8a53403a5b148ebb2dcd2ee3e075fe3c5aacdf3a
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-18 23:18:08 +0100

    Update test.js

--- test.js
+++ test.js
@@ -391,7 +391,22 @@ client.on('guildMemberAdd', (member) => {
   member.addRole(member.guild.roles.find('name', 'not active'));
   });
    
+  client.on('message', message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+      if (command === "bans") { // الامر
+          message.delete(5000)
+           if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.reply("Error : \` I Dont Have ADMINISTRATOR Permission\`").then(message => message.delete(5000));
+          if(!message.member.hasPermission('ADMINISTRATOR')) return;
+          if(!message.channel.guild) return;
+          message.guild.fetchBans()
+          .then(bans => message.channel.send(`\`${bans.size}\` ***: عدد الاشخاص المبندين من السيرفر ***`)).then(message => message.delete(5000))
    
+    .catch(console.error);
+  }
+  });
+  
+  
   client.on('message', message => {                      
       if(!message.channel.guild) return;
          if(message.content.startsWith(prefix + 'active')) {
@@ -438,28 +453,13 @@ client.on('ready', () => {
   });
 });
 
-client.on('message', message => {
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-    if (command === "bans") { // الامر
-        message.delete(5000)
-         if(!message.guild.member(client.user).hasPermission("ADMINISTRATOR")) return message.reply("Error : \` I Dont Have ADMINISTRATOR Permission\`").then(message => message.delete(5000));
-        if(!message.member.hasPermission('ADMINISTRATOR')) return;
-        if(!message.channel.guild) return;
-        message.guild.fetchBans()
-        .then(bans => message.channel.send(`\`${bans.size}\` ***: عدد الاشخاص المبندين من السيرفر ***`)).then(message => message.delete(5000))
- 
-  .catch(console.error);
-}
-});
-
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "s_chat");
-     stewart.send(`<@${member.user.id}> **By:**  <@${inviter.id}>`);
+     stewart.send(`<@${member.user.id}> **invite By:**  <@${inviter.id}>`);
     // stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
 });
