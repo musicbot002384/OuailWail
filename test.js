945552832e78e62abdeb8278dc6bfbbb18eb94ef
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-19 21:41:59 +0100

    Update test.js

--- test.js
+++ test.js
@@ -56,46 +56,18 @@ if (message.content.startsWith(prefix + 'setgame')) {
 
 });
 
-client.on('message', message => {
-  if(!message.channel.guild) return;
-if(message.content.startsWith('+bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );  //OUAIL
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let copy = "DgPro-BC";
-let request = `Requested By ${message.author.username}`;  //OUAIL
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))  //OUAIL
-.then(() =>msg.react('✅'))  //OUAIL
-
-
-
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });  //OUAIL
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });  //OUAIL
-reaction1.on("collect", r => {
-message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));  //OUAIL
-message.guild.members.forEach(m => {
-var bc = new  
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  //OUAIL
-.addField('الرساله', args)  //OUAIL
-.setThumbnail(message.author.avatarURL)  //OUAIL
-.setFooter(copy, client.user.avatarURL); //OUAIL
-m.send({ embed: bc })
-msg.delete();  //OUAIL
-})
-})
-reaction2.on("collect", r => {  //OUAIL
-message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));   //OUAIL
-msg.delete();  //OUAIL
-})  //OUAIL
-}) //OUAIL
-}  //OUAIL
-}) //OUAIL
+client.on("message", message => {
+    if (message.content.startsWith("%bc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
 
 client.on('message', message=> {
   if (message.author.bot) return;
@@ -415,28 +387,6 @@ client.on('message', message => {
        
     }
     }
-})
-
-client.on('message', function(message) {
-  if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-          let command = message.content.split(" ")[0];
-      if(message.content.includes('youtube.com')){
-      message.reply (' ')
-         if(!message.channel.guild) return message.reply('** This command only for servers**');
-   message.member.addRole(message.guild.roles.find('name', 'Muted'));
-  const embed500 = new Discord.RichEmbed()
-    .setTitle(":x: | تمت معاقبتك")
-          .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر روابط فيديو اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-    .addField(`by`, `[  + ${message.author.tag} +  ]`)
-          .setColor("c91616")
-          .setThumbnail(`${message.author.avatarURL}`)
-          .setAuthor(message.author.username, message.author.avatarURL)
-      .setFooter(`${message.guild.name} Server`)
-   message.channel.send(embed500)
- 
-     
-  }
-  }
 })
 
 client.on('guildMemberAdd', (member) => {
