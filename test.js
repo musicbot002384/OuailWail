f4b2e6e1fbe05168ed028fa988008ded85766939
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-20 18:42:59 +0100

    Update test.js

--- test.js
+++ test.js
@@ -116,8 +116,7 @@ client.on('message', function(msg) {
 client.on('guildMemberAdd', msg => { 
     var embed = new Discord.RichEmbed()
     .setAuthor(msg.user.username, msg.user.avatarURL)
-    .setThumbnail(msg.user.avatarURL)
-    .setImage('رابط صوره')     
+    .setThumbnail(msg.user.avatarURL)   
     .setTitle('New Member!')
     .setDescription('Welcome To server')
     .addField('**ID Member:',"" +  msg.user.id, true)
@@ -130,6 +129,20 @@ client.on('guildMemberAdd', msg => {
     if (!channel) return;
     channel.send({embed : embed});
     });
+
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
 
 client.on('message', async message => {
   let args = message.content.split(" ");
