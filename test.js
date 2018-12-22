76943f80571d5a0b2bd0267db57fa70587c76171
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-22 01:03:44 +0100

    Update test.js

--- test.js
+++ test.js
@@ -62,7 +62,6 @@ if (message.content.startsWith(prefix + 'setgame')) {
 client.on('message', message => {
   if (message.author.bot) return;
    if (message.content === prefix + "help") {
-    if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
    message.channel.send('**تم ارسال رسالة في الخاص**');
 
 
