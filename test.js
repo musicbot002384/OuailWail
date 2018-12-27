636c93e7c2902934abc4309bcd0c52feaa4a7a62
OuailYT <42055439+OuailYT@users.noreply.github.com>
2018-12-27 03:52:40 +0100

    Update test.js

--- test.js
+++ test.js
@@ -316,7 +316,7 @@ message.channel.send("** تم انشاء روم القبول والرفض بنج
 })
 
 client.on("guildMemberAdd", m => {
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 16) {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
         m.ban();
     };
     function parseDate(str) {
