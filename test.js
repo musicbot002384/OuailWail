c02dba6456cbb7c11c0b9815be284dee1e466894
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-27 18:49:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -794,34 +794,6 @@ message.channel.send({embed});
   }
 });
 
-const invites = {};
-
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-  wait(500);
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "chat-space");
-     stewart.send(`<@${member.user.id}> **invite By:**  <@${inviter.id}>`);
-     stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  })
-});
-// ////////////////////////////////////////////////////////////////
-// ///////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////
-// //////////////////////////////////////////
-// ////////////////////////////////
 client.on("guildMemberAdd", (member) => {
   let channel = member.guild.channels.find('name', 'welcome'); //OUAIL
   if(!channel) {
