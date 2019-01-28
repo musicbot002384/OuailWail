91c7749415a1bf02609c1e042eafe6c20d1f9553
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-28 18:49:52 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,37 +3,68 @@ const client = new Discord.Client();
 const prefix = "%"
 client.on('ready', () => {
     console.log('I am ready!');
-client.user.setGame(`test-2`,"https://www.twitch.tv/peery13")
+client.user.setGame(`test-3`,"https://www.twitch.tv/peery13")
 client.user.setStatus("dnd")
 }); //OUAIL
  
-client.on("guildMemberAdd", (member) => {
-  let channel = member.guild.channels.find('name', 'chat-sharks');
-  if(!channel) {
-    console.log("!channel fails");
-    return;
-  }
-  if(member.id === client.user.id) {
-    return;
-  }
-  console.log('made it till here!');
-    var guild;
-    while (!guild)
-    guild = member.guild
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(`**By : ${Invite.inviter}  **`)            
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
+client.on('guildMemberAdd',async member => {
+  const Canvas = require('canvas');
+  const jimp = require('jimp');
+  const w = ['./welcome_4.png'];
+        let Image = Canvas.Image,
+            canvas = new Canvas(800, 300),
+            ctx = canvas.getContext('2d');
+        ctx.patternQuality = 'bilinear';
+        ctx.filter = 'bilinear';
+        ctx.antialias = 'subpixel';
+        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+        ctx.shadowOffsetY = 2;
+        ctx.shadowBlur = 2;
+        ctx.stroke();
+        ctx.beginPath();
+ 
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 800, 300);
+ 
+})
+ 
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                 if (err) return console.log(err);
+ 
+          ctx.font = '36px Arial';
+          ctx.fontSize = '72px';
+          ctx.fillStyle = "#ffffff";
+          ctx.textAlign = "center";
+          ctx.fillText(member.user.username, 545, 177);
+         
+          ctx.font = '16px Arial Bold';
+          ctx.fontSize = '72px';
+          ctx.fillStyle = "#ffffff";
+          ctx.textAlign = "center";
+          ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
+         
+          let Avatar = Canvas.Image;
+          let ava = new Avatar;
+          ava.src = buf;
+          ctx.beginPath();
+          ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
+          ctx.closePath();
+          ctx.clip();
+          ctx.drawImage(ava, 36, 21, 260, 260);
+           
+    let channel = member.guild.channels.find('name', 'chat-sharks');
+          client.sendFile(canvas.toBuffer());
+ 
 });
-
+});
+});
 
 
 client.login(process.env.BOT_TOKEN);
