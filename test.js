b7cc589beb7523595757b82bcbf157add2fa5b5b
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-28 19:59:28 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,13 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const Canvas = require('canvas');
 const jimp = require('jimp');
-const prefix = "%"
-client.on('ready', () => {
-    console.log('I am ready!');
-client.user.setGame(`test-3`,"https://www.twitch.tv/peery13")
-client.user.setStatus("dnd")
-}); //OUAIL
- 
+
 client.on('guildMemberAdd',async member => {
   const Canvas = require('canvas');
   const jimp = require('jimp');
@@ -61,12 +55,39 @@ client.on('guildMemberAdd',async member => {
           ctx.clip();
           ctx.drawImage(ava, 36, 21, 260, 260);
            
-    let channel = member.guild.channels.find('name', 'chat-sharks');
+    let channel = member.guild.channels.find('name', 'welcome');
           client.sendFile(canvas.toBuffer());
  
 });
 });
 });
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    if(!channel) {
+      console.log("!channel fails");
+      return;
+    }
+    if(member.id === client.user.id) {
+      return;
+    }
+    console.log('made it till here!');
+      var guild;
+      while (!guild)
+      guild = member.guild
+      guild.fetchInvites().then((data) => {
+          data.forEach((Invite, key, map) => {
+              var Inv = Invite.code;
+              if (dat[Inv])
+                  if (dat[Inv] < Invite.uses) {
+                      console.log(3);
+                      console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+   channel.send(`**By : ${Invite.inviter}  **`)            
+   }
+              dat[Inv] = Invite.uses;
+          })
+      })
+  });
 
 
 client.login(process.env.BOT_TOKEN);
