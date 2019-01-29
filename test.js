d1dd55b7d12eee02490e4d032d66bbc20f4b0b99
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-29 18:21:37 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,70 +2,10 @@ const Discord = require('discord.js');
 const moment = require("moment");
 const fs = require('fs');
 const Canvas = require('canvas');
-const dat = require("dat");
 const jimp = require('jimp');
 const client = new Discord.Client();
 const prefix = '%'
 
-client.on('guildMemberAdd',async member => {
-  const Canvas = require('canvas');
-  const jimp = require('jimp');
-  const w = ['./welcome_4.png'];
-        let Image = Canvas.Image,
-            canvas = new Canvas(800, 300),
-            ctx = canvas.getContext('2d');
-        ctx.patternQuality = 'bilinear';
-        ctx.filter = 'bilinear';
-        ctx.antialias = 'subpixel';
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 2;
-        ctx.stroke();
-        ctx.beginPath();
- 
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-            if (err) return console.log(err);
-            let BG = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 800, 300);
- 
-})
- 
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                 if (err) return console.log(err);
- 
-          ctx.font = '36px Arial';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(member.user.username, 545, 177);
-         
-          ctx.font = '16px Arial Bold';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
-         
-          let Avatar = Canvas.Image;
-          let ava = new Avatar;
-          ava.src = buf;
-          ctx.beginPath();
-          ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.clip();
-          ctx.drawImage(ava, 36, 21, 260, 260);
-           
-    let channel = member.guild.channels.find('name', 'welcome');
-          client.sendFile(canvas.toBuffer());
- 
-});
-});
-});
- 
 client.on('message', message => {
 var prefix = "%"
     if (message.content.startsWith(prefix + 'clear')) {
@@ -96,6 +36,10 @@ var prefix = "%"
     let prefix = "%";
 let args = message.content.split(" ").slice(1).join(" ");
 if(message.content.startsWith(prefix + "say")) {
+	if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('You don\'t have permission').then(msg => {
+       msg.delete(4500);
+       message.delete(4500);
+    });
 if(!args) return;
 message.channel.send(`**# ${args}**`);
 }
@@ -127,7 +71,7 @@ ${args}
 client.on('ready', () => {
   console.log(`Welcome To Me ${client.user.tag}!`);
   console.log('----------------');
-  client.user.setGame(`Sharks Bot`,"https://www.twitch.tv/peery13");
+  client.user.setGame(`Sharks Bot %help`,"https://www.twitch.tv/peery13");
       client.user.setStatus("dnd");
 });
 
@@ -148,7 +92,8 @@ client.on("message", message => {
          %help : عرض هذه الرسالة
          %report : التبليغ عن مخالفة او أي شيء
          ======={ADMIN COMMAD}=======
-%clear : مسح الشات
+         %say : البوت يعيد كلامك
+         %clear : مسح الشات
          %cl : تقفل الشات
          %op : تفتح الشات
          -----------------------------
