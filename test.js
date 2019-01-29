cef2ea9620dcac2ae829f4798db92503454f5146
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-29 22:11:14 +0100

    Update test.js

--- test.js
+++ test.js
@@ -6,6 +6,39 @@ const jimp = require('jimp');
 const client = new Discord.Client();
 const prefix = '%'
 
+client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "welcome");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+     //stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  });
+})
+
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.find('name', 'welcome');
+  
+    const millis = new Date().getTime() - member.user.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+
+
+
+
+  
+    const embed = new Discord.RichEmbed()
+    
+    .setColor("black")
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
+    .setAuthor(member.user.tag, member.user.avatarURL);
+    channel.sendEmbed(embed);
+
+  
+});
+
 client.on('message', message => {
 var prefix = "%"
     if (message.content.startsWith(prefix + 'clear')) {
