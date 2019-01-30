6dd0d1a287da94d1afcd738e724a126c74af54d4
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-30 14:16:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -6,16 +6,18 @@ const jimp = require('jimp');
 const client = new Discord.Client();
 const prefix = '%'
 
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-     //stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  });
-})
+var roles = {};
+client.on('guildMemberRemove', member => {
+ roles[member.id] = {roles: member.roles.array()};
+});
+client.on('guildMemberAdd', member => {
+if(!roles[member.user.id]) return;
+console.log(roles[member.user.id].roles.length);
+for(let i = 0; i < roles[member.user.id].roles.length; i++) {
+member.addRole(roles[member.user.id].roles);
+roles.shift();
+}
+});
 
 client.on('guildMemberAdd', member => {
 
