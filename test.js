63ed2f2bd99d07e4cb97bc00f810942e61743ec0
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-01-30 23:12:16 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const moment = require("moment");
 const fs = require('fs');
+const util = require ('util');
 const Canvas = require('canvas');
 const jimp = require('jimp');
 const client = new Discord.Client();
